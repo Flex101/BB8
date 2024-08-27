ce1d9e8daf12565922e3088be4cd5d1e8d351523
Matthew Goodliffe <msgoodliffe@gmail.com>
2024-08-27 09:22:40 +0100

    Moved to Playstation Navigation Controller input Checks for both controllers (ball and dome) on start.

--- Controller/controller.py
+++ Controller/controller.py
@@ -6,40 +6,36 @@ import struct
 import time
 import os
 
+PRINT = False   # Enabling this will reduce performance
+
+class ControllerNames:
+	ball = "PLAYSTATION(R)3 Controller (04:76:6E:99:8D:EF)"
+	dome = "PLAYSTATION(R)3 Controller (00:26:43:C9:DD:9E)"
 
 class GameController(object):
 
-	controller = None
-	axis_data = None
-	button_data = None
-	hat_data = None
+	def __init__(self):
+		self.controller = None
+		self.axis_data = None
+		self.button_data = None
+		self.hat_data = None
 
-	def init(self):
+	def connect(self, index:int=0):
 		"""Initialize the joystick components"""
-
-		pygame.init()
-		pygame.joystick.init()
-		self.controller = pygame.joystick.Joystick(0)
+		self.controller = pygame.joystick.Joystick(index)
 		self.controller.init()
-
 		self.button_data = [False] * self.controller.get_numbuttons()
 		self.hat_data = [(0, 0)] * self.controller.get_numhats()
-		self.axis_data = [0.0] * self.controller.get_numaxes()
+		self.axis_data = [0.0] * 2 #self.controller.get_numaxes()
 
 	def setAxisCenter(self, axisIndex:int, center:float):
 		self.axis_data[axisIndex] = center
 
 	def update(self):
-		for event in pygame.event.get():
-			if event.type == pygame.JOYAXISMOTION:
-				self.axis_data[event.axis] = event.value
-			elif event.type == pygame.JOYBUTTONDOWN:
-				self.button_data[event.button] = True
-			elif event.type == pygame.JOYBUTTONUP:
-				self.button_data[event.button] = False
-			elif event.type == pygame.JOYHATMOTION:
-				self.hat_data[event.hat] = event.value
-
+		for i in range(len(self.axis_data)):
+			self.axis_data[i] = self.controller.get_axis(i)
+		for i in range(len(self.button_data)):
+			self.button_data[i] = self.controller.get_button(i)
 
 def filterAxis(value:float, center:float, deadzone:float, invert:bool):
 	
@@ -70,18 +66,63 @@ def buildPacket(controller:GameController, axis:int):
 	return packet
 
 
-controller = GameController()
-controller.init()
-controller.setAxisCenter(1, 0.2)
+pygame.init()
+pygame.joystick.init()
+controllerCount = pygame.joystick.get_count()
+print(f"Controllers found: {controllerCount}")
+
+controllers = []
+for i in range(pygame.joystick.get_count()):
+	controller = GameController()
+	controller.connect(i)
+	controller.setAxisCenter(1, 0.2)
+	controllers.append(controller)
+
+ballController = None
+domeController = None
+
+for controller in controllers:
+	match controller.controller.get_name():
+		case ControllerNames.ball:
+			ballController = controller
+			continue
+		case ControllerNames.dome:
+			domeController = controller
+			continue
+
+if (ballController == None):
+	print("Couldn't find ball controller")
+if (domeController == None):
+	print("Couldn't find dome controller")
+if (ballController == None or domeController == None):
+	exit()
+print("Found ball and dome controllers")
 
 serialPortRoll = serial.Serial('/dev/rfcomm0')
 serialPortTilt = serial.Serial('/dev/rfcomm1')
 
-
 while (True):
-	controller.update()
-	packet = buildPacket(controller, 1)
+	pygame.event.get()
+	ballController.update()
+	domeController.update()
+
+	if PRINT:
+		print("Ball:", end=' ')
+		for i in range(len(ballController.axis_data)):
+			print("{:.3f}".format(ballController.axis_data[i]).rjust(6), end=' ')
+		for i in range(len(ballController.button_data)):
+			print(ballController.button_data[i], end=' ')
+		print()
+
+		print("Dome:", end=' ')
+		for i in range(len(domeController.axis_data)):
+			print("{:.3f}".format(domeController.axis_data[i]).rjust(6), end=' ')
+		for i in range(len(domeController.button_data)):
+			print(domeController.button_data[i], end=' ')
+		print()
+
+	packet = buildPacket(ballController, 1)
 	serialPortRoll.write(packet)
-	packet = buildPacket(controller, 0)
+	packet = buildPacket(ballController, 0)
 	serialPortTilt.write(packet)
 	time.sleep(0.01)
