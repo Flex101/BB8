77e1e59df6b6489e7d8997d7b04b61ea8cbd5555
Flex101 <matt_goodliffe@hotmail.com>
2026-08-23 19:19:47 -0400

    Controller - added bluetooth serial port connection script

---
+++ Controller/connect.sh
@@ -0,0 +1,5 @@
+sudo rfcomm unbind rfcomm0 28:CD:C1:08:DE:50 1
+sudo rfcomm unbind rfcomm1 28:CD:C1:0F:07:95 1
+
+sudo rfcomm bind rfcomm0 28:CD:C1:08:DE:50 1
+sudo rfcomm bind rfcomm1 28:CD:C1:0F:07:95 1
