67f20458399986561bd1f2daf341ef121305b53e
Flex101 <matt_goodliffe@hotmail.com>
2026-08-23 19:36:53 -0400

    Controller - restore unbind for cleaner cycle

--- Controller/connect.sh
+++ Controller/connect.sh
@@ -1,5 +1,8 @@
 sudo fuser -k /dev/rfcomm0
 sudo fuser -k /dev/rfcomm1
 
+sudo rfcomm unbind rfcomm0 28:CD:C1:08:DE:50 1
+sudo rfcomm unbind rfcomm1 28:CD:C1:0F:07:95 1
+
 sudo rfcomm bind rfcomm0 28:CD:C1:08:DE:50 1
 sudo rfcomm bind rfcomm1 28:CD:C1:0F:07:95 1
