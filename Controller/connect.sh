b0d845227c05e1654f02f5068827b0e55d2f9308
Flex101 <matt_goodliffe@hotmail.com>
2026-09-02 10:34:22 -0400

    Connect - Added dome board

--- Controller/connect.sh
+++ Controller/connect.sh
@@ -1,8 +1,11 @@
 sudo fuser -k /dev/rfcomm0
 sudo fuser -k /dev/rfcomm1
+sudo fuser -k /dev/rfcomm2
 
 sudo rfcomm unbind rfcomm0 28:CD:C1:08:DE:50 1
 sudo rfcomm unbind rfcomm1 28:CD:C1:0F:07:95 1
+sudo rfcomm unbind rfcomm2 28:CD:C1:0F:07:83 1
 
 sudo rfcomm bind rfcomm0 28:CD:C1:08:DE:50 1
 sudo rfcomm bind rfcomm1 28:CD:C1:0F:07:95 1
+sudo rfcomm bind rfcomm1 28:CD:C1:0F:07:83 1
